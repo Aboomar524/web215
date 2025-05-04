5ba6e661917fe03185a7f097b22f1b6dc6c007ab
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-05-04 01:28:08 -0400

    ش

--- birthday-tracker/client/src/index.js
+++ birthday-tracker/client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import App from './App';
 import Login from './Login';
 import Register from './Register';
