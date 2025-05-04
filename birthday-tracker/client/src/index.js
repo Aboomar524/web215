e36848135cf09b4bb3382dd2f6ed846dc62e25c0
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-05-04 01:18:23 -0400

    a

--- birthday-tracker/client/src/index.js
+++ birthday-tracker/client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './Login';
 import Register from './Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
+    <BrowserRouter basename="/web215/birthday-tracker-app">
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
         </Routes>
-    </Router>
+    </BrowserRouter>
 );
--- birthday-tracker/server/models/User.js
+++ birthday-tracker/server/models/User.js
@@ -1,9 +1,21 @@
-// server/models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+// تشفير كلمة المرور قبل حفظها
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// دالة مقارنة كلمة المرور عند تسجيل الدخول
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
